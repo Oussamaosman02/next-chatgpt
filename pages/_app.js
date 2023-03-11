a4f0cbf7ed9ced9f78f6335687861243bfe8ce09
oussama02 <oussamaosman02@gmail.com>
2023-03-11 09:23:31 +0100

    Adding matomo analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '@/styles/globals.css'
+import Script from 'next/script'
 
 export default function App ({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id='matomo' strategy='afterInteractive'>
+        {`
+  var _paq = window._paq = window._paq || []
+  _paq.push(['trackPageView'])
+  _paq.push(['enableLinkTracking'])
+  (function() {
+    var u='https://creal.matomo.cloud/'
+    _paq.push(['setTrackerUrl', u+'matomo.php'])
+    _paq.push(['setSiteId', '1'])
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]
+    g.async=true g.src='//cdn.matomo.cloud/creal.matomo.cloud/matomo.js' s.parentNode.insertBefore(g,s)
+  })()
+    `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
